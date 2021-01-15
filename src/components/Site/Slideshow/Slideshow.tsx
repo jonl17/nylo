c23715996f37c76b6565c475f6b086869f8946e7
jglorange <jglorange@gmail.com>
2021-01-15 16:44:56 +0000

    slideshow work

--- src/components/Site/Slideshow/Slideshow.tsx
+++ src/components/Site/Slideshow/Slideshow.tsx
@@ -2,17 +2,7 @@ import React, { useEffect, useMemo, useState } from 'react'
 
 const Slideshow = ({ images }: { images: { url: string; alt: string }[] }) => {
   const [index, setIndex] = useState(0)
-  const [direction, setDirection] = useState<'up' | 'down'>('up')
   const [positions, setPositions] = useState([0])
-
-  const handleClick = () => {
-    setIndex(prevIndex => (direction === 'up' ? prevIndex + 1 : prevIndex - 1))
-  }
-
-  useMemo(() => {
-    if (index === images.length - 1) setDirection('down')
-    if (index === 0) setDirection('up')
-  }, [index])
 
   useEffect(() => {
     setTimeout(() => {
@@ -22,7 +12,6 @@ const Slideshow = ({ images }: { images: { url: string; alt: string }[] }) => {
       const nodes = Array.from(
         document.querySelectorAll('.slideshow-image')
       ).map(node => {
-        console.dir(node.clientWidth)
         return node.clientWidth
       })
 
@@ -38,23 +27,35 @@ const Slideshow = ({ images }: { images: { url: string; alt: string }[] }) => {
   }, [])
 
   return (
-    <div
-      style={{
-        transform: `translateX(-${positions[index]}px)`,
-        cursor: direction === 'up' ? 'e-resize' : 'w-resize',
-      }}
-      onClick={() => handleClick()}
-      className='my-3 d-flex slideshow'
-    >
-      {images.map((image, idx) => (
-        <img
-          id={`image-${idx}`}
-          className='slideshow-image'
-          key={image.url}
-          src={image.url}
-          alt={image.alt}
+    <div className='position-relative'>
+      <div
+        style={{
+          transform: `translateX(-${positions[index]}px)`,
+        }}
+        className='my-3 d-flex slideshow'
+      >
+        {images.map((image, idx) => (
+          <img
+            id={`image-${idx}`}
+            className='slideshow-image'
+            key={image.url}
+            src={image.url}
+            alt={image.alt}
+          />
+        ))}
+      </div>
+      {index > 0 && (
+        <button
+          onClick={() => setIndex(prev => prev - 1)}
+          className='slideshow__clickzone slideshow__clickzone--left removeGenericButtonStyles'
         />
-      ))}
+      )}
+      {index < images.length - 1 && (
+        <button
+          onClick={() => setIndex(prev => prev + 1)}
+          className='slideshow__clickzone slideshow__clickzone--right removeGenericButtonStyles'
+        />
+      )}
     </div>
   )
 }
