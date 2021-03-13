884e2c056c6820b44a43c182aec4da183996cd2f
jglorange <jglorange@gmail.com>
2021-03-13 13:02:10 +0000

    update sort fix

--- src/components/Slices/LatestEvents/LatestEvents.tsx
+++ src/components/Slices/LatestEvents/LatestEvents.tsx
@@ -6,9 +6,12 @@ import { langSeek } from 'balkan-tungumal'
 import { langs } from 'prismic.config'
 
 const LatestEvents = ({ lang }: { lang: Language }) => {
-  const events = useGetEvents()
-    .filter(node => node.lang === lang)
-    .slice(0, 2)
+  const tdy = new Date()
+  const events = useGetEvents().filter(node => {
+    if (node.lang === lang && new Date(node.date) > tdy) {
+      return node
+    }
+  })
 
   if (!events) {
     return null
@@ -17,15 +20,18 @@ const LatestEvents = ({ lang }: { lang: Language }) => {
   return (
     <div className='latest-events col-lg-6 p-0'>
       <h1 className='latest-events__heading'>{langSeek('Events', lang)}</h1>
-      {events.map((ev, ent) => (
-        <div key={ent} className='mb-4'>
-          <div className='d-flex mb-0'>
-            <h1 className='mr-3'>{formatDate(ev.date, undefined, true)}</h1>
-            <h1>{ev.time}</h1>
-          </div>
-          <h1>{ev.name.text}</h1>
-        </div>
-      ))}
+      {events.map(
+        (ev, ent) =>
+          ent < 2 && (
+            <div key={ent} className='mb-4'>
+              <div className='d-flex mb-0'>
+                <h1 className='mr-3'>{formatDate(ev.date, undefined, true)}</h1>
+                <h1>{ev.time}</h1>
+              </div>
+              <h1>{ev.name.text}</h1>
+            </div>
+          )
+      )}
     </div>
   )
 }
